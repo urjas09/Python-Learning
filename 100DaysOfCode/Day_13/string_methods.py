f15127f933114a971bc52b972078bb64f8f25ff0
Urja Singh <urjas0906@gmail.com>
2026-06-08 00:48:53 +0530

    Day 13 : String Methods in Python

---
+++ 100DaysOfCode/Day_13/string_methods.py
@@ -0,0 +1,16 @@
+#String Methods
+#Strings are immutable in Python, which means that once a string is created, it cannot be modified. However, Python provides a variety of built-in string methods that allow you to perform operations on strings and create new strings based on the original.
+#Here are some commonly used string methods in Python:
+#1. `upper()`: Converts all characters in the string to uppercase.
+a = "Urja"
+print(a.upper()) #we cannot change the original string but we can create a copy of the string with the modified value and whenever we use the methods it gives us a new string where all the letters will be in uppercase.
+#2. `lower()`: Converts all characters in the string to lowercase.
+b = "URJA"
+print(b.lower()) #we cannot change the original string but we can create a copy of the string with the modified value and whenever we use the methods it gives us a new string where all the letters will be in lowercase.
+#Basically strings work on our existing string and gives us a new string with the modified value.
+#3. `rstrip()`: Removes any trailing characters (characters at the end of a string), based on the argument passed. If no argument is passed, it removes trailing whitespace.
+c= "Urja!!!!"
+print(c.rstrip("!")) #we cannot change the original string but we can create a copy of the string with the modified value and whenever we use the methods it gives us a new string where all the trailing exclamation marks will be removed.
+#4. `replace()`: Replaces a specified phrase with another specified phrase. It returns a new string with the replacements.
+print(a.replace("Urja", "Kajal")) #we cannot change the original string but we can create a copy of the string with the modified value and whenever we use the methods it gives us a new string where all the occurrences of "Urja" will be replaced with "Kajal".
+print(a) #the original string remains unchanged.
